f6ea684af111ef0aaa75b121049002963a1301c8
Javier Rivera <javier.rivera.electronics@gmail.com>
2023-08-02 14:30:38 -0500

    Se realiza el pre reto 2, como el ejercicio es basico, se realiza la parte del toast, pero se usan alerts para mostrar los datos validados

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView Nombre,Direccion,Telefono,Correo;
+    private RadioButton Granja,Apartamnto,Casa;
+    private CheckBox Psolar, Aegen, Otros;
+    private Button Enviar;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Nombre = (TextView) findViewById(R.id.txtNombre);
+        Direccion = (TextView) findViewById(R.id.txtDireccion);
+        Telefono = (TextView) findViewById(R.id.txtTelefono);
+        Correo = (TextView) findViewById(R.id.txtCorreoE);
+        Granja = (RadioButton) findViewById(R.id.rbGranja);
+        Apartamnto = (RadioButton) findViewById(R.id.rbApartamento);
+        Casa = (RadioButton) findViewById(R.id.rbCasa);
+        Psolar = (CheckBox) findViewById(R.id.chPsolar);
+        Aegen = (CheckBox) findViewById(R.id.chAegen);
+        Otros = (CheckBox) findViewById(R.id.chOtros);
+        Enviar = (Button) findViewById(R.id.btnEnviar);
+
+        Enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Nombre.getText().toString().isEmpty() || Direccion.getText().toString().isEmpty()
+                || Telefono.getText().toString().isEmpty() || Correo.getText().toString().isEmpty()){
+                    Comprobar();
+                } else{
+                    Ingreso();
+                }
+            }
+        });
+
+        Casa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Sleccionaste Casa", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Granja.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Sleccionaste Granja", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Apartamnto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Sleccionaste Apartamento", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void Comprobar(){
+        AlertDialog.Builder OjoManito = new AlertDialog.Builder(MainActivity.this);
+        OjoManito.setMessage("Debes llenar los campos!!!").setCancelable(false)
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog titulo = OjoManito.create();
+        titulo.setTitle("Ojo con eso manito");
+        titulo.show();
+    }
+
+    public void Ingreso(){
+        AlertDialog.Builder OjoManito = new AlertDialog.Builder(MainActivity.this);
+        OjoManito.setMessage("Nombre: " + Nombre.getText().toString()
+                +"\nDireccion: " + Direccion.getText().toString() + "\nTelefono: " + Telefono.getText().toString()
+                + "\nCorreo: " + Correo.getText().toString()).setCancelable(false)
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog titulo = OjoManito.create();
+        titulo.setTitle("Has ingresado:");
+        titulo.show();
+    }
+}
